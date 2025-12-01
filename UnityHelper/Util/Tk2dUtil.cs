44204ffec82a1c9c3fc70934c7bf75d4a39066fa
kaycodes13 <egad.coding@gmail.com>
2025-11-30 21:33:56 -0500

    add utilities for creating new tk2d animations

---
+++ UnityHelper/Util/Tk2dUtil.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+using WrapMode = tk2dSpriteAnimationClip.WrapMode;
+
+namespace Silksong.UnityHelper.Util;
+
+/// <summary>
+/// Class containing utility and extension methods for creating tk2d animations.
+/// </summary>
+public static class Tk2dUtil
+{
+
+    /// <summary>
+    /// Creates a <see cref="tk2dSpriteAnimationClip"/> from a collection of sprites.
+    /// </summary>
+    /// <param name="name">The name of the animation clip.</param>
+    /// <param name="fps">The speed of the animation in frames per second.</param>
+    /// <param name="frames">
+    ///     A collection of sprites to use as the frames of the animation.
+    ///     Each sprite must have a unique <see cref="UObject.name"/>.
+    /// </param>
+    /// <param name="wrapMode">
+    ///     The wrap mode for the animation.
+    ///     This determines if and how the animation loops.
+    /// </param>
+    /// <param name="loopStart">
+    ///     The frame index that each loop of a looping animation starts at.
+    /// </param>
+    /// <param name="pixelsPerUnit">
+    ///     The in-game resolution for all sprites in the collection. Higher values scale
+    ///     them down, lower values scale them up.
+    /// </param>
+    /// <returns>A <see cref="tk2dSpriteAnimationClip"/> object.</returns>
+    public static tk2dSpriteAnimationClip CreateTk2dAnimationClip(
+        string name,
+        int fps,
+        IEnumerable<Texture2D> frames,
+        WrapMode wrapMode = WrapMode.Once,
+        int loopStart = 0,
+        float pixelsPerUnit = 64f
+    ) {
+        var spriteCollection = CreateTk2dSpriteCollection(
+            frames, pixelsPerUnit: pixelsPerUnit
+        );
+        return new() {
+            name = name,
+            fps = fps,
+            wrapMode = wrapMode,
+            loopStart = loopStart,
+            frames = spriteCollection.CreateFrames([.. frames.Select(x => x.name)]),
+        };
+    }
+
+    /// <inheritdoc cref="CreateTk2dAnimationClip(string, int, IEnumerable{Texture2D}, WrapMode, int, float)"/>
+    public static tk2dSpriteAnimationClip CreateTk2dAnimationClip(
+        string name,
+        int fps,
+        IEnumerable<Sprite> frames,
+        WrapMode wrapMode = WrapMode.Once,
+        int loopStart = 0,
+        float pixelsPerUnit = 64f
+    ) {
+        var spriteCollection = CreateTk2dSpriteCollection(
+            frames, pixelsPerUnit: pixelsPerUnit
+        );
+        return new() {
+            name = name,
+            fps = fps,
+            wrapMode = wrapMode,
+            loopStart = loopStart,
+            frames = spriteCollection.CreateFrames([.. frames.Select(x => x.name)]),
+        };
+    }
+
+    /// <summary>
+    /// Create a <see cref="tk2dSpriteCollectionData" /> from a collection of sprites.
+    /// Sprite collections can be used to create <see cref="tk2dSpriteAnimationFrame"/>s
+    /// for use in <see cref="tk2dSpriteAnimationClip"/>s.
+    /// </summary>
+    /// <param name="sprites">The sprites to construct a collection from.</param>
+    /// <param name="spriteNames">
+    ///     Unique names to be assigned to each sprite in the collection.
+    ///     If unset, each sprite's <see cref="UObject.name"/> field is used.
+    /// </param>
+    /// <param name="spriteCenters">
+    ///     The center or pivot point, in pixels, of each sprite in the collection.
+    ///     If unset, the center point of each sprite is used.
+    /// </param>
+    /// <param name="pixelsPerUnit">
+    ///     The in-game resolution for all sprites in the collection. Higher values scale
+    ///     them down, lower values scale them up.
+    /// </param>
+    /// <returns>A <see cref="tk2dSpriteCollectionData" /> object.</returns>
+    public static tk2dSpriteCollectionData CreateTk2dSpriteCollection(
+        IEnumerable<Texture2D> sprites,
+        IEnumerable<string>? spriteNames = null,
+        IEnumerable<Vector2>? spriteCenters = null,
+        float pixelsPerUnit = 64f
+    ) {
+        spriteNames ??= sprites.Select(x => x.name);
+        spriteCenters ??= sprites.Select(x => new Vector2(x.width / 2f, x.height / 2f));
+
+        #region Error Checking
+        int frameCount = sprites.Count();
+        if (frameCount != spriteNames.Count())
+        {
+            throw new ArgumentException($"Number of {nameof(spriteNames)} does not match number of {nameof(sprites)}", nameof(spriteNames));
+        }
+        if (frameCount != spriteCenters.Count())
+        {
+            throw new ArgumentException($"Number of {nameof(spriteCenters)} does not match number of {nameof(sprites)}", nameof(spriteCenters));
+        }
+
+        if (spriteNames.Any(string.IsNullOrWhiteSpace))
+        {
+            throw new ArgumentException($"Sprite names must not be null, empty, or only whitespace.", nameof(spriteNames));
+        }
+        #endregion
+
+        // Make sure all incoming textures are readable
+        Texture2D[] readableSprites = [.. sprites.Select(SpriteUtil.GetReadableCopyOfTexture)];
+
+        // Make a basic spritesheet
+        Texture2D atlas = new(1, 1);
+        Rect[] spriteRegions = atlas.PackTextures(readableSprites, 0);
+        atlas.Apply(false, makeNoLongerReadable: true);
+
+        for (int i = 0; i < spriteRegions.Length; i++) {
+            Rect region = spriteRegions[i];
+
+            // Sprite regions are in percentages of atlas dimensions,
+            // but we need them in pixels
+            region.x *= atlas.width;
+            region.y *= atlas.height;
+            region.width *= atlas.width;
+            region.height *= atlas.height;
+
+            // Also need them flipped vertically because unity and tk2d
+            // disagree on whether Y starts at the top or the bottom
+            region.y = atlas.height - region.yMax;
+
+            spriteRegions[i] = region;
+        }
+
+        var spriteCollection = tk2dSpriteCollectionData.CreateFromTexture(
+            atlas,
+            tk2dSpriteCollectionSize.PixelsPerMeter(pixelsPerUnit),
+            [.. spriteNames],
+            spriteRegions,
+            [.. spriteCenters]
+        );
+
+        UObject.DontDestroyOnLoad(atlas);
+        UObject.DontDestroyOnLoad(spriteCollection);
+
+        return spriteCollection;
+    }
+
+    /// <inheritdoc cref="CreateTk2dSpriteCollection(IEnumerable{Texture2D}, IEnumerable{string}?, IEnumerable{Vector2}?, float)" path="//*[not(self::param[@name='spriteCenters'])]"/>
+    /// <param name="spriteCenters">
+    ///     The center or pivot point, in pixels, of each sprite in the collection.
+    ///     If unset, the <see cref="Sprite.pivot"/> field of each sprite is used.
+    /// </param>
+    public static tk2dSpriteCollectionData CreateTk2dSpriteCollection(
+        #pragma warning disable CS1573
+        IEnumerable<Sprite> sprites,
+        IEnumerable<string>? spriteNames = null,
+        IEnumerable<Vector2>? spriteCenters = null,
+        float pixelsPerUnit = 64f
+        #pragma warning restore CS1573
+    ) =>
+        CreateTk2dSpriteCollection(
+            sprites.Select(x => x.texture),
+            spriteNames ?? sprites.Select(x => x.name),
+            spriteCenters ?? sprites.Select(x => x.pivot),
+            pixelsPerUnit
+        );
+
+    /// <summary>
+    /// Create a <see cref="tk2dSpriteAnimationFrame"/> from a sprite in this collection.
+    /// </summary>
+    /// <param name="collection">The collection to pull the sprite from.</param>
+    /// <param name="spriteName">The name of the sprite to use.</param>
+    /// <param name="triggerEvent">
+    ///     Whether or not this frame triggers an animation event.
+    /// </param>
+    /// <returns>A <see cref="tk2dSpriteAnimationFrame"/> object.</returns>
+    public static tk2dSpriteAnimationFrame CreateFrame(
+        this tk2dSpriteCollectionData collection,
+        string spriteName,
+        bool triggerEvent = false
+    ) =>
+        new() {
+            spriteCollection = collection,
+            spriteId = collection.GetSpriteIdByName(spriteName),
+            triggerEvent = triggerEvent
+        };
+
+    /// <summary>
+    /// Create a collection of <see cref="tk2dSpriteAnimationFrame"/>s from multiple
+    /// sprites in this collection.
+    /// </summary>
+    /// <param name="collection">The collection to pull sprites from.</param>
+    /// <param name="spriteNames">The names of the sprites to use.</param>
+    /// <returns>An array of <see cref="tk2dSpriteAnimationFrame"/> objects.</returns>
+    public static tk2dSpriteAnimationFrame[] CreateFrames(
+        this tk2dSpriteCollectionData collection,
+        params string[] spriteNames
+    ) =>
+        [.. spriteNames.Select(x => collection.CreateFrame(x))];
+
+}
