c719930a7f94615de1f65d98926cec0febc0545d
Kaykao <32107308+kaycodes13@users.noreply.github.com>
2025-12-02 16:24:34 -0500

    Add utilities for TK2D animations (#6)
    * add GetReadableCopyOfTexture function
    
    * add utilities for creating new tk2d animations
    
    * add optional params for Sprite pivot point and texture readability
    
    * adjust atlas padding & error messages in tk2dutil
    
    * sprite utils now set name fields of Sprites and Texture2Ds
    
    This smooths the process of making animations, since the tk2d utils by default need sprites/textures to be named.
    
    * add a CreateFrames overload that accepts an IEnumerable
    
    * add sprite util overloads to avoid breaking change
    
    * sprite utils now inherit docstrings for common parameters
    
    * clarify tk2d util docs with regard to sprite uniqueness
    
    * add texture import functions to SpriteUtil
    
    * add tk2d util usage examples to README

--- UnityHelper/Util/SpriteUtil.cs
+++ UnityHelper/Util/SpriteUtil.cs
@@ -10,14 +10,163 @@ namespace Silksong.UnityHelper.Util;
 /// </summary>
 public static class SpriteUtil
 {
+
+    #region Sprites
+
     /// <summary>
     /// Load an image from the assembly's embedded resources, and return a Sprite.
     /// </summary>
     /// <param name="asm">The assembly to load from.</param>
     /// <param name="path">The path to the image.</param>
-    /// <param name="pixelsPerUnit">The pixels per unit. Changing this value will scale the size of the sprite accordingly.</param>
+    /// <inheritdoc
+    ///     cref="LoadSpriteFromArray(byte[], float, Vector2?, bool)"
+    ///     path="//param" />
     /// <returns>A Sprite object.</returns>
-    public static Sprite LoadEmbeddedSprite(Assembly asm, string path, float pixelsPerUnit = 64f)
+    public static Sprite LoadEmbeddedSprite(
+        Assembly asm, string path,
+        #pragma warning disable CS1573
+        float pixelsPerUnit = 64f, Vector2? pivot = null, bool makeUnreadable = false
+        #pragma warning restore CS1573
+    ) {
+        byte[] buffer = GetEmbeddedImageData(asm, path);
+
+        Sprite result = LoadSpriteFromArray(buffer, pixelsPerUnit, pivot, makeUnreadable);
+        result.name = result.texture.name = path;
+
+        return result;
+    }
+
+    /// <inheritdoc cref="LoadEmbeddedSprite(Assembly, string, float, Vector2?, bool)" />
+    public static Sprite LoadEmbeddedSprite(Assembly asm, string path, float pixelsPerUnit)
+        => LoadEmbeddedSprite(asm, path, pixelsPerUnit, null);
+
+    /// <summary>
+    /// Load an image from a file on disc and return a Sprite.
+    /// </summary>
+    /// <param name="fileName">The path to the image file.</param>
+    /// <inheritdoc
+    ///     cref="LoadSpriteFromArray(byte[], float, Vector2?, bool)"
+    ///     path="//param" />
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static Sprite LoadSpriteFromFile(
+        string fileName,
+        #pragma warning disable CS1573
+        float pixelsPerUnit = 64f, Vector2? pivot = null, bool makeUnreadable = false
+        #pragma warning restore CS1573
+    ) {
+        byte[] fileBytes = GetFileImageData(fileName);
+
+        Sprite result = LoadSpriteFromArray(fileBytes, pixelsPerUnit, pivot, makeUnreadable);
+        result.name = result.texture.name = Path.GetFileNameWithoutExtension(fileName);
+        return result;
+    }
+
+    /// <inheritdoc cref="LoadSpriteFromFile(string, float, Vector2?, bool)"/>
+    public static Sprite LoadSpriteFromFile(string fileName, float pixelsPerUnit)
+        => LoadSpriteFromFile(fileName, pixelsPerUnit, null);
+
+    /// <summary>
+    /// Create a sprite from a byte array.
+    /// </summary>
+    /// <param name="buffer"></param>
+    /// <param name="pixelsPerUnit">The pixels per unit. Changing this value will scale the size of the sprite accordingly.</param>
+    /// <param name="pivot">The pivot point of the resulting Sprite. Defaults to the center.</param>
+    /// <param name="makeUnreadable">Whether or not to mark the Sprite's underlying texture as unreadable.</param>
+    /// <returns></returns>
+    public static Sprite LoadSpriteFromArray(byte[] buffer, float pixelsPerUnit = 64f, Vector2? pivot = null, bool makeUnreadable = false)
+    {
+        Texture2D tex = LoadTextureFromArray(buffer, makeUnreadable);
+        return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), pivot ?? Vector2.one * 0.5f, pixelsPerUnit);
+    }
+
+    /// <inheritdoc cref="LoadSpriteFromArray(byte[], float, Vector2?, bool)"/>
+    public static Sprite LoadSpriteFromArray(byte[] buffer, float pixelsPerUnit)
+        => LoadSpriteFromArray(buffer, pixelsPerUnit, null);
+
+    #endregion
+
+    #region Texture2D
+
+    /// <summary>
+    /// Load an image from the assembly's embedded resources and return a texture.
+    /// </summary>
+    /// <param name="asm">The assembly to load from.</param>
+    /// <param name="path">The path to the image.</param>
+    /// <param name="makeUnreadable">Whether or not to mark the texture as unreadable.</param>
+    /// <returns>A <see cref="Texture2D"/> object.</returns>
+    public static Texture2D LoadEmbeddedTexture(Assembly asm, string path, bool makeUnreadable = false)
+    {
+        byte[] buffer = GetEmbeddedImageData(asm, path);
+
+        Texture2D result = LoadTextureFromArray(buffer, makeUnreadable);
+        result.name = path;
+        return result;
+    }
+
+    /// <summary>
+    /// Load an image from a file on disc and return a texture.
+    /// </summary>
+    /// <param name="fileName">The path to the image file.</param>
+    /// <param name="makeUnreadable">Whether or not to mark the texture as unreadable.</param>
+    /// <returns>A <see cref="Texture2D"/> object.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static Texture2D LoadTextureFromFile(string fileName, bool makeUnreadable = false)
+    {
+        byte[] buffer = GetFileImageData(fileName);
+
+        Texture2D result = LoadTextureFromArray(buffer, makeUnreadable);
+        result.name = Path.GetFileNameWithoutExtension(fileName);
+        return result;
+    }
+
+    /// <summary>
+    /// Create a texture from a byte array.
+    /// </summary>
+    /// <param name="buffer">The raw image data.</param>
+    /// <param name="makeUnreadable">Whether or not to mark the texture as unreadable.</param>
+    /// <returns>A <see cref="Texture2D"/> object.</returns>
+    public static Texture2D LoadTextureFromArray(byte[] buffer, bool makeUnreadable = false)
+    {
+        Texture2D tex = new(2, 2);
+        tex.LoadImage(buffer, makeUnreadable);
+        return tex;
+    }
+
+    /// <summary>
+    /// If the given texture is unreadable, returns a readable copy of it.
+    /// If the given texture is readable, returns the same texture object.
+    /// </summary>
+    /// <param name="tex">The texture to make readable.</param>
+    /// <returns>A readable <see cref="Texture2D"/> object.</returns>
+    public static Texture2D GetReadableTexture(Texture2D tex) {
+        if (tex.isReadable)
+            return tex;
+
+        var temp = RenderTexture.GetTemporary(
+            tex.width, tex.height, 0,
+            RenderTextureFormat.Default, RenderTextureReadWrite.Linear
+        );
+        Graphics.Blit(tex, temp);
+
+        var previous = RenderTexture.active;
+        RenderTexture.active = temp;
+
+        var readable = new Texture2D(tex.width, tex.height);
+        readable.ReadPixels(new Rect(0, 0, temp.width, temp.height), 0, 0);
+        readable.Apply();
+
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(temp);
+
+        return readable;
+    }
+
+    #endregion
+
+    #region Local utilities
+
+    private static byte[] GetEmbeddedImageData(Assembly asm, string path)
     {
         using Stream stream = asm.GetManifestResourceStream(path);
 
@@ -31,17 +180,10 @@ public static class SpriteUtil
                 """);
         }
 
-        return LoadSpriteFromArray(buffer, pixelsPerUnit);
+        return buffer;
     }
 
-    /// <summary>
-    /// Load an image from a file on disc and return a Sprite.
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="pixelsPerUnit"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentException"></exception>
-    public static Sprite LoadSpriteFromFile(string fileName, float pixelsPerUnit = 64f)
+    private static byte[] GetFileImageData(string fileName)
     {
         if (string.IsNullOrWhiteSpace(fileName))
         {
@@ -53,23 +195,9 @@ public static class SpriteUtil
             throw new ArgumentException($"File {fileName} not found", nameof(fileName));
         }
 
-        byte[] fileBytes = File.ReadAllBytes(fileName);
-
-        return LoadSpriteFromArray(fileBytes, pixelsPerUnit);
+        return File.ReadAllBytes(fileName);
     }
 
-    /// <summary>
-    /// Create a sprite from a byte array.
-    /// </summary>
-    /// <param name="buffer"></param>
-    /// <param name="pixelsPerUnit"></param>
-    /// <returns></returns>
-    public static Sprite LoadSpriteFromArray(byte[] buffer, float pixelsPerUnit = 64f)
-    {
-        Texture2D tex = new(2, 2);
+    #endregion
 
-        tex.LoadImage(buffer, true);
-
-        return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f, pixelsPerUnit);
-    }
 }
